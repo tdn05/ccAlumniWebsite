2b473047893281c8e22b6c42b7689ab6abbe4a1b
tdn05 <tduynguyen05@gmail.com>
2016-10-17 10:34:04 -0700

    First Commit

---
+++ ngApp/controllers/controllers.ts
@@ -0,0 +1,12 @@
+namespace ccalummiwebsite.Controllers {
+
+    export class HomeController {
+        public message = 'Hello from the home page!';
+    }
+
+
+    export class AboutController {
+        public message = 'Hello from the about page!';
+    }
+
+}
